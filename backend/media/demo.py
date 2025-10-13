62acff209b6684c8a0d42254b3fd2c24e24dbdf4
nikoparas1 <nikoparas1@gmail.com>
2025-10-13 14:40:19 -0400

    latest changes

--- backend/media/demo.py
+++ backend/media/demo.py
@@ -1,26 +1,7 @@
 from typing import *
 
 def countComponents(n: int, edges: List[List[int]]) -> int:
-    graph = {i: [] for i in range(n)}
-    for a, b in edges:
-        graph[a].append(b)
-        graph[b].append(a)
-    visited = set()
-    components = 0
-    
-    def dfs(node):
-        if node in visited:
-            return
-        visited.add(node)
-        for neighbor in graph[node]:
-            dfs(neighbor)
-    
-    for i in range(n):
-        if i not in visited:
-            dfs(i)
-            components += 1
-    
-    return components
+        return 0
 def run_test(n, edges, expected):
     exception = ""
     result = ""
