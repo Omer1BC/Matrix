e288c236af4b4a0875f6592bb8adbde5028d1c53
omer18 <omer18@vt.edu>
2025-12-04 16:36:56 -0500

    blank commit

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -20,6 +20,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 USER_FILES = BASE_DIR / "user_files"
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
